3ddd85edfe3266ab4af4f1556175c4b0a4bbd8a7
lauravangorp <lauravangorp@users.noreply.github.com>
2020-07-08 16:42:26 +0200

    1

---
+++ port/script.js
@@ -0,0 +1,21 @@
+// hier komt je code
+// console.log("Hallo wereld!");
+
+// fixed header
+window.addEventListener("scroll", function() {
+      let scrollPositiePx = window.pageYOffset;
+      console.log(scrollPositiePx);
+      if (scrollPositiePx > 160) {
+            document.querySelector("header").classList.add("fixed");
+      } else {
+            document.querySelector("header").classList.remove("fixed");
+      };
+})
+
+let hamburgerIcon = document.querySelector("header div.hamburger");
+let header = document.querySelector("header");
+
+hamburgerIcon.addEventListener("click", function () {
+	this.classList.toggle("actief");
+	header.classList.toggle("uitgeklapt");
+})
---
+++ white/script.js
@@ -0,0 +1,30 @@
+window.addEventListener("scroll", function() {
+      let scrollPositiePx = window.pageYOffset;
+      console.log(scrollPositiePx);
+      if (scrollPositiePx > 160) {
+            document.querySelector("header").classList.add("fixed");
+      } else {
+            document.querySelector("header").classList.remove("fixed");
+      };
+})
+
+let hamburgerIcon = document.querySelector("header div.hamburger");
+let header = document.querySelector("header");
+
+hamburgerIcon.addEventListener("click", function () {
+	this.classList.toggle("actief");
+	header.classList.toggle("uitgeklapt");
+})
+
+
+let darkLink = document.querySelectorAll("nav a")[0];
+let lightLink = document.querySelectorAll("nav a")[1];
+let mijnCSS = document.querySelector("#switchcss");
+
+darkLink.addEventListener("click", function () {
+	mijnCSS.setAttribute("href","../style.css");
+});
+
+lightLink.addEventListener("click", function () {
+	mijnCSS.setAttribute("href","style.css");
+});
